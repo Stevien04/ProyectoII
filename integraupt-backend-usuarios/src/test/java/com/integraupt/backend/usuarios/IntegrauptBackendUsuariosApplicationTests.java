8fabba6ed284811b3ef17b2c8b83562534a19932
Stevien04 <Stemarcaa@upt.pe>
2025-10-25 16:52:15 -0500

    sma

---
+++ integraupt-backend-usuarios/src/test/java/com/integraupt/backend/usuarios/IntegrauptBackendUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integraupt.backend.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrauptBackendUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
